3c1f2f8fc81c382b4388ee9921b70c2d4a71f0ab
Rhythmic System <mc.adhdmc@gmail.com>
2025-06-15 14:52:22 -0700

    bump version

--- src/main/java/simplexity/simplepronouns/dependencies/PronounPlaceholders.java
+++ src/main/java/simplexity/simplepronouns/dependencies/PronounPlaceholders.java
@@ -22,7 +22,7 @@ public class PronounPlaceholders extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "2.0.0";
+        return "2.0.1";
     }
 
     @Override
